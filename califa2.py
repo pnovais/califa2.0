5d0bcab75b8049a9bda4a7ca40ac446a95a3c232
Patricia Novais <patynovais@gmail.com>
2016-12-09 08:23:39 -0200

    2nd day

--- califa2.py
+++ califa2.py
@@ -52,6 +52,49 @@ def get_image(f_sdss):
 #    sky = f_sdss[2].data
     return img
 
+data_dir = '/home/pnovais/Dropbox/DOUTORADO/renew'
+age = pd.read_csv('Paty_at_flux__yx/age.csv')
+mass = pd.read_csv('PatImages/mass.csv')
+halpha = pd.read_csv('Hamaps/halpha.csv')
+
+
+for i_gal in range(len(halpha)):
+#for i_gal in range(0,3):
+    print(bcolors.FAIL +'-'*79+ bcolors.ENDC)
+    print(bcolors.FAIL + '-'*33 + 'OBJETO: %s' %halpha['num_gal'][i_gal] + '-'*33 + bcolors.ENDC)
+    print(bcolors.FAIL +'-'*79+ bcolors.ENDC)
+    plt.close()
+    image_ha = fits.open('Hamaps/%s_%s_Ha.fits' %(halpha['num_gal'][i_gal],halpha['type'][i_gal]))
+    img = get_image(image_ha)
+
+    #plotando a imagem fits
+    plt.figure(1)
+    plt.clf()
+    cx = cubehelix.cmap(reverse=True, start=0., rot=-0.5)
+    plt.axis([0,77,0,72])
+    plt.xlabel('X',fontweight='bold')
+    plt.ylabel('Y',fontweight='bold')
+    imgplot = plt.imshow(100*np.log10(img/255), cmap=cx)
+    titulo='Galaxy %s ' %halpha['num_gal'][i_gal]
+    plt.title(titulo)
+    #plt.colorbar()
+    figura = 'figures/galaxy_%s' %age['num_gal'][i_gal]
+    plt.savefig(figura)
+
+    #obtendo os dados da imagem fits
+    df_ha = pd.DataFrame()
+    nrows, ncols = img.shape
+    xx, yy = np.meshgrid( *np.ogrid[:ncols, :nrows] )
+    table = np.column_stack(( xx.flatten(), yy.flatten(), img.flatten() ))
+    temp = pd.DataFrame(table, columns=['x','y','halpha'])
+    df_ha = pd.concat([df_ha,temp], axis=1)
+
+    #selecionando apenas os dados de idade > 0 e mass > 0
+    df = df_ha[(df_ha.halpha > 0.0)]
+
+
+
+
 
 
 fim = time.time()
